9cbadff94a1fdee80f3d8174b5d3a2f4cc942293
ykoh <kenshin579@gmail.com>
2018-10-17 21:13:19 +0900

    결과값 Set으로 변경함

--- src/main/java/com/google/CommonSet.java
+++ src/main/java/com/google/CommonSet.java
@@ -3,10 +3,10 @@ package com.google;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CommonSet {
     private static final Logger LOG = LoggerFactory.getLogger(CommonSet.class);
@@ -18,19 +18,18 @@ public class CommonSet {
      * @param B
      * @return
      */
-    public List<Integer> solution(int[] A, int[] B) {
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Boolean> mapA = new HashMap<>();
+    public Set<Integer> solution(int[] A, int[] B) {
+        Set<Integer> result = new HashSet<>();
+        Map<Integer, Boolean> mapB = new HashMap<>();
 
-        //O(N)
-        for (int x : A) {
-            if (!mapA.containsKey(x)) {
-                mapA.put(x, true);
+        for (int x : B) {
+            if (!mapB.containsKey(x)) { //중복되는 값은 제거
+                mapB.put(x, true);
             }
         }
 
-        for (int x : B) {
-            if (mapA.containsKey(x)) {
+        for (int x : A) {
+            if (mapB.containsKey(x)) {
                 result.add(x);
             }
         }
--- src/test/java/com/google/CommonSetTest.java
+++ src/test/java/com/google/CommonSetTest.java
@@ -5,7 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,12 +14,22 @@ public class CommonSetTest {
     private static final Logger LOG = LoggerFactory.getLogger(CommonSetTest.class);
 
     @Test
-    public void test_find_common_valuves() {
+    public void test_find_common_values() {
         int[] A = {1, 1, 1, 1, 2, 2};
         int[] B = {3, 3, 4, 1, 2};
 
-        List<Integer> expectedArray = Arrays.asList(1, 2);
-        List<Integer> result = new CommonSet().solution(A, B);
-        assertEquals(expectedArray, result);
+        Set<Integer> expectedResult = new HashSet<>(Arrays.asList(1, 2));
+        Set<Integer> result = new CommonSet().solution(A, B);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void test_find_common_values2() {
+        int[] A = {2, 7, 1, 4, 5, 6, 9, 8, 7};
+        int[] B = {4, 6, 8, 2, 3, 5, 3, 1};
+
+        Set<Integer> expectedResult = new HashSet<>(Arrays.asList(4, 6, 8, 2, 5, 1));
+        Set<Integer> result = new CommonSet().solution(A, B);
+        assertEquals(expectedResult, result);
     }
 }
