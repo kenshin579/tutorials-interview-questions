f3e8f1fa3eb6e300c1e13051503161716f660f51
frankyoh <frankyoh@tmon.co.kr>
2019-08-19 16:55:57 +0900

    added a problem convert decimal to any base number

---
+++ src/main/java/com/java/examples/bitoperation/ConvertDeciToAnyBase.java
@@ -0,0 +1,15 @@
+package com.java.examples.bitoperation;
+
+public class ConvertDeciToAnyBase {
+
+	/**
+	 * https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+	 *
+	 * @param n
+	 * @param base
+	 * @return
+	 */
+	public static String convertToAnyBase(int n, int base) {
+		return "";
+	}
+}
