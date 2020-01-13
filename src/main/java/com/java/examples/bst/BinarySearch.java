eaca36fe1709536c700986306357bfa301f3bf87
frankyoh <frankyoh@tmon.co.kr>
2020-01-13 23:18:54 +0900

    Binary Search 소스 업데이트

--- src/main/java/com/java/examples/bst/BinarySearch.java
+++ src/main/java/com/java/examples/bst/BinarySearch.java
@@ -2,30 +2,28 @@ package com.java.examples.bst;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Binary Search 조건
+ * - array는 정렬이 되어 있어야 함
+ * <p>
+ * 결과
+ * - 검색한 index를 반환함
+ */
 @Slf4j
 public class BinarySearch {
 
-	/**
-	 * 이건 recursive로 디자인됨.
-	 *
-	 * @param priceArray
-	 * @param start
-	 * @param end
-	 * @param searchNumber
-	 * @return
-	 */
-	public static int RecursiveBinarySeach(int[] priceArray, int start, int end, int searchNumber) {
+	public static int RecursiveBinarySeach(int[] numArr, int start, int end, int searchNumber) {
 		if (end >= start) {
 			int mid = start + (end - start) / 2;
 
-			if (priceArray[mid] == searchNumber) {
+			if (numArr[mid] == searchNumber) {
 				return mid;
 			}
 
-			if (priceArray[mid] > searchNumber) {
-				return RecursiveBinarySeach(priceArray, start, mid - 1, searchNumber);
+			if (numArr[mid] > searchNumber) {
+				return RecursiveBinarySeach(numArr, start, mid - 1, searchNumber);
 			}
-			return RecursiveBinarySeach(priceArray, mid + 1, end, searchNumber);
+			return RecursiveBinarySeach(numArr, mid + 1, end, searchNumber);
 		}
 		return -1;
 	}
@@ -34,20 +32,20 @@ public class BinarySearch {
 	 * https://www.youtube.com/watch?v=j5uXyPJ0Pew
 	 * non recursive 방식임.
 	 *
-	 * @param priceArray
-	 * @param searchPrice
+	 * @param numArr
+	 * @param searchNumber
 	 * @return
 	 */
-	public static int nonrecursiveBinarySearch(int[] priceArray, int searchPrice) {
+	public static int nonrecursiveBinarySearch(int[] numArr, int searchNumber) {
 		int startIndex = 0;
-		int endIndex = priceArray.length - 1;
+		int endIndex = numArr.length - 1;
 		int midIndex;
 		while (startIndex <= endIndex) {
 			midIndex = (startIndex + endIndex) / 2;
 
-			if (priceArray[midIndex] == searchPrice) {
+			if (numArr[midIndex] == searchNumber) {
 				return midIndex;
-			} else if (searchPrice < priceArray[midIndex]) {
+			} else if (searchNumber < numArr[midIndex]) {
 				endIndex = midIndex - 1;
 			} else {
 				startIndex = midIndex + 1;
--- src/test/java/com/java/examples/bst/BinarySearchTest.java
+++ src/test/java/com/java/examples/bst/BinarySearchTest.java
@@ -4,29 +4,48 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class BinarySearchTest {
-    int priceCount = 4000;
+    private final int MAX_ARRAY = 10;
 
-    // 가격정보 배열
-    int[] priceArray = new int[priceCount];
+    int[] numArr = new int[MAX_ARRAY];
+    int searchNum;
 
     @Before
     public void setUp() throws Exception {
-        for (int priceArrayIdx = 1; priceArrayIdx < priceCount; priceArrayIdx++) {
-            priceArray[priceArrayIdx - 1] = priceArrayIdx * 1000;
+        List<Integer> temp = new ArrayList();
+
+        for (int i = 1; i < MAX_ARRAY; i++) {
+            temp.add(getRandomNumber(50));
         }
+        searchNum = temp.get(this.getRandomNumber(numArr.length));
+        Collections.sort(temp);
+        //https://stackoverflow.com/questions/718554/how-to-convert-an-arraylist-containing-integers-to-primitive-int-array
+        numArr = temp.stream().mapToInt(Integer::intValue).toArray();
+        log.info("searchNum : {} numArr : {}", searchNum, numArr);
+
     }
 
     @Test
-    public void recurisve_test() {
-        assertThat(BinarySearch.RecursiveBinarySeach(priceArray, 0, priceArray.length, 690000)).isEqualTo(689);
+    public void test_recursive() {
+        assertThat(BinarySearch.RecursiveBinarySeach(numArr, 0, numArr.length, searchNum)).isNotNegative();
     }
 
     @Test
-    public void non_recurisve_test() {
-        assertThat(BinarySearch.nonrecursiveBinarySearch(priceArray, 690000)).isEqualTo(689);
+    public void test_non_recursive() {
+        int index = BinarySearch.nonrecursiveBinarySearch(numArr, searchNum);
+        log.info("index :{}", index);
+        assertThat(index).isNotNegative();
+    }
+
+    private int getRandomNumber(int max) {
+        return new Random().nextInt(max);
     }
 }
