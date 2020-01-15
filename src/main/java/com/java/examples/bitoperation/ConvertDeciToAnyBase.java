25534410f4a327a3b980843901bf7afefbfc0029
frankyoh <frankyoh@tmon.co.kr>
2020-01-15 22:20:51 +0900

    N 진수로 convert 소스 업데이트

--- src/main/java/com/java/examples/bitoperation/ConvertDeciToAnyBase.java
+++ src/main/java/com/java/examples/bitoperation/ConvertDeciToAnyBase.java
@@ -1,8 +1,12 @@
 package com.java.examples.bitoperation;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ConvertDeciToAnyBase {
 
 	/**
+	 * 기록:|
 	 * https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
 	 *
 	 * @param n
@@ -10,6 +14,13 @@ public class ConvertDeciToAnyBase {
 	 * @return
 	 */
 	public static String convertToAnyBase(int n, int base) {
-		return "";
+		StringBuilder sb = new StringBuilder();
+		int remainder;
+		while (n > 0) {
+			remainder = n % base;
+			n = n / base;
+			sb.insert(0, remainder);
+		}
+		return sb.toString();
 	}
 }
--- src/test/java/com/java/examples/bitoperation/ConvertDeciToAnyBaseTest.java
+++ src/test/java/com/java/examples/bitoperation/ConvertDeciToAnyBaseTest.java
@@ -10,11 +10,20 @@ public class ConvertDeciToAnyBaseTest {
 
 	/**
 	 * https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
-	 *
+	 * <p>
 	 * 10 -> base: 2 : 1010
 	 */
 	@Test
-	public void convertDeciToAnyBase() {
-		assertThat(ConvertDeciToAnyBase.convertToAnyBase(10, 2)).isEqualTo("1010");
+	public void convertDeciToAnyBase1() {
+		int n = 10;
+		int base = 2;
+		assertThat(ConvertDeciToAnyBase.convertToAnyBase(n, base)).isEqualTo(Integer.toString(n, base));
+	}
+
+	@Test
+	public void convertDeciToAnyBase2() {
+		int n = 300;
+		int base = 8;
+		assertThat(ConvertDeciToAnyBase.convertToAnyBase(n, base)).isEqualTo(Integer.toString(n, base));
 	}
 }
