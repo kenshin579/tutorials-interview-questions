249c7c886182b2718852f6d26b6d5a6f4e6628d4
Frank Oh <frankyoh@tmon.co.kr>
2020-02-17 20:07:11 +0900

    leetcode 추가

---
+++ src/main/java/com/leetcode/algorithm/TwoSum.java
@@ -0,0 +1,26 @@
+package com.leetcode.algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ */
+@Slf4j
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] results = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                log.info("[{}][{}] {}, {}", i, j, nums[i], nums[j]);
+                if (target == nums[i] + nums[j]) {
+                    results[0] = i;
+                    results[1] = j;
+                    break;
+                }
+
+            }
+        }
+        return results;
+    }
+}
