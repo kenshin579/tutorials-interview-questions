8a4d698ecb35eb8ec4c9a3761059a11e7a4ecbef
frankyoh <frankyoh@tmon.co.kr>
2020-02-18 23:33:12 +0900

    leetcode IsBalanced 추가

---
+++ src/main/java/com/leetcode/algorithm/easy/IsBalanced.java
@@ -0,0 +1,10 @@
+package com.leetcode.algorithm.easy;
+
+/**
+ * https://leetcode.com/problems/balanced-binary-tree/
+ */
+public class IsBalanced {
+    public boolean isBalanced(TreeNode root) {
+        return false;
+    }
+}
---
+++ src/test/java/com/leetcode/algorithm/easy/IsBalancedTest.java
@@ -0,0 +1,27 @@
+package com.leetcode.algorithm.easy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class IsBalancedTest {
+
+    @Test
+    public void test_isBalanced1() {
+        //[3,9,20,null,null,15,7]
+        TreeNode treeNode = new TreeNode(3);
+
+        assertThat(new IsBalanced().isBalanced(treeNode)).isTrue();
+    }
+
+    @Test
+    public void test_isBalanced2() {
+        //[1,2,2,3,3,null,null,4,4]
+        TreeNode treeNode = new TreeNode(3);
+
+        assertThat(new IsBalanced().isBalanced(treeNode)).isTrue();
+    }
+
+}
