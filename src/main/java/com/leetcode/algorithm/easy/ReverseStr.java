552f620f85a65492d279c59b2314c54f50ec958e
frankyoh <frankyoh@tmon.co.kr>
2020-02-25 23:11:09 +0900

    leetcode ReverseStr 완료

--- src/main/java/com/leetcode/algorithm/easy/ReverseStr.java
+++ src/main/java/com/leetcode/algorithm/easy/ReverseStr.java
@@ -1,10 +1,34 @@
 package com.leetcode.algorithm.easy;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
+ * 2k 블록마다 reverse 하는 예제임
+ * ex.
+ * 12345678901234567890
+ * ___456___012___679__
+ * 32145698701254367809
+ * <p>
  * https://leetcode.com/problems/reverse-string-ii/
  */
+@Slf4j
 public class ReverseStr {
     public String reverseStr(String s, int k) {
-        return null;
+        char[] chars = s.toCharArray();
+        int length = s.length();
+        char temp;
+        int startIndex, endIndex;
+        for (int start = 0; start < length; start += 2 * k) {
+            startIndex = start;
+            endIndex = Math.min(start + k - 1, length - 1);
+            while (startIndex < endIndex) {
+                temp = chars[startIndex];
+                chars[startIndex] = chars[endIndex];
+                chars[endIndex] = temp;
+                startIndex++;
+                endIndex--;
+            }
+        }
+        return new String(chars);
     }
 }
--- src/test/java/com/leetcode/algorithm/easy/ReverseStrTest.java
+++ src/test/java/com/leetcode/algorithm/easy/ReverseStrTest.java
@@ -8,6 +8,41 @@ public class ReverseStrTest {
 
     @Test
     public void test_reverseStr1() {
-        assertThat(new ReverseStr().reverseStr("abcdefg", 2)).isEqualTo("bacdefg");
+        assertThat(new ReverseStr().reverseStr("abcdefg", 2)).isEqualTo("bacdfeg");
+    }
+
+    @Test
+    public void test_reverseStr2() {
+        assertThat(new ReverseStr().reverseStr("abcdefg", 3)).isEqualTo("cbadefg");
+    }
+
+    @Test
+    public void test_reverseStr3() {
+        assertThat(new ReverseStr().reverseStr("abcdefg", 1)).isEqualTo("abcdefg");
+    }
+
+    @Test
+    public void test_reverseStr1_str이_k보다_적은_경우() {
+        assertThat(new ReverseStr().reverseStr("abcdefg", 5)).isEqualTo("edcbafg");
+    }
+
+    @Test
+    public void test_reverseStr1_str이_k와_같은_경우() {
+        assertThat(new ReverseStr().reverseStr("abcdefg", 7)).isEqualTo("gfedcba");
+    }
+
+    @Test
+    public void test_reverseStr_숫자_테스트1() {
+        assertThat(new ReverseStr().reverseStr("12345678901234567890", 2)).isEqualTo("21346578091243568790");
+    }
+
+    @Test
+    public void test_reverseStr_숫자_테스트2() {
+        assertThat(new ReverseStr().reverseStr("12345678901234567890", 3)).isEqualTo("32145698701254367809");
+    }
+
+    @Test
+    public void test_reverseStr_숫자_테스트3() {
+        assertThat(new ReverseStr().reverseStr("12345678901234567890", 4)).isEqualTo("43215678210934560987");
     }
 }
