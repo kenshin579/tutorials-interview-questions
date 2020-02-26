3efee5a32d307ec4aa1e321f660c732024256c63
frankyoh <frankyoh@tmon.co.kr>
2020-02-26 23:04:43 +0900

    leetcode GetDecimalValue 추가

---
+++ src/main/java/com/leetcode/algorithm/easy/GetDecimalValue.java
@@ -0,0 +1,12 @@
+package com.leetcode.algorithm.easy;
+
+import com.leetcode.algorithm.common.struct.ListNode;
+
+/**
+ * https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+ */
+public class GetDecimalValue {
+    public int getDecimalValue(ListNode head) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/leetcode/algorithm/easy/GetDecimalValueTest.java
@@ -0,0 +1,12 @@
+package com.leetcode.algorithm.easy;
+
+import com.leetcode.algorithm.common.struct.LinkedList;
+import org.junit.Test;
+
+public class GetDecimalValueTest {
+
+    @Test
+    public void getDecimalValue() {
+        LinkedList linkedList = new LinkedList();
+    }
+}
