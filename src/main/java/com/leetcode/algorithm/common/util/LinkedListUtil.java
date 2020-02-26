8ddd7cd5507f2543d6ec8d47cefabbaeb1bce65b
frankyoh <frankyoh@tmon.co.kr>
2020-02-26 23:06:38 +0900

    leetcode LinkedListUtil 추가

--- src/main/java/com/leetcode/algorithm/common/util/LinkedListUtil.java
+++ src/main/java/com/leetcode/algorithm/common/util/LinkedListUtil.java
@@ -1,5 +1,14 @@
 package com.leetcode.algorithm.common.util;
 
+import com.leetcode.algorithm.common.struct.LinkedList;
+
 public final class LinkedListUtil {
 
+    public static LinkedList generateLinkedList(int max) {
+        LinkedList linkedList = new LinkedList();
+        for (int i = 0; i < max; i++) {
+            linkedList.insertNode(i + 1);
+        }
+        return linkedList;
+    }
 }
--- src/test/java/com/leetcode/algorithm/easy/MiddleNodeTest.java
+++ src/test/java/com/leetcode/algorithm/easy/MiddleNodeTest.java
@@ -1,6 +1,7 @@
 package com.leetcode.algorithm.easy;
 
 import com.leetcode.algorithm.common.struct.LinkedList;
+import com.leetcode.algorithm.common.util.LinkedListUtil;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,24 +10,16 @@ public class MiddleNodeTest {
 
     @Test
     public void middleNode() {
-        LinkedList linkedList = this.generateLinkedList(5);
+        LinkedList linkedList = LinkedListUtil.generateLinkedList(5);
         linkedList.printList();
         assertThat(new MiddleNode().middleNode(linkedList.head).val).isEqualTo(3);
     }
 
     @Test
     public void middleNode2() {
-        LinkedList linkedList = this.generateLinkedList(2);
+        LinkedList linkedList = LinkedListUtil.generateLinkedList(2);
         linkedList.printList();
 
         assertThat(new MiddleNode().middleNode(linkedList.head).val).isEqualTo(2);
-    }
-
-    private LinkedList generateLinkedList(int max) {
-        LinkedList linkedList = new LinkedList();
-        for (int i = 0; i < max; i++) {
-            linkedList.insertNode(i + 1);
-        }
-        return linkedList;
     }
 }
