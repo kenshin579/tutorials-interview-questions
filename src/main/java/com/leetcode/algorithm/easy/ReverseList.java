c81b050e919947de37c30b75a3406d3d75916c2b
frankyoh <frankyoh@tmon.co.kr>
2020-02-27 22:40:55 +0900

    leetcode ReverseList 완료

--- src/main/java/com/leetcode/algorithm/easy/ReverseList.java
+++ src/main/java/com/leetcode/algorithm/easy/ReverseList.java
@@ -1,6 +1,7 @@
 package com.leetcode.algorithm.easy;
 
 import com.leetcode.algorithm.common.struct.ListNode;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Definition for singly-linked list.
@@ -10,9 +11,26 @@ import com.leetcode.algorithm.common.struct.ListNode;
  * ListNode(int x) { val = x; }
  * }
  * https://leetcode.com/problems/reverse-linked-list/
+ * https://www.educative.io/courses/coderust-hacking-the-coding-interview/lq2j
  */
+@Slf4j
 public class ReverseList {
     public ListNode reverseList(ListNode head) {
-        return null;
+        ListNode tempNode;
+        ListNode reverseNode = head;
+        ListNode traverseNode = head;
+
+        if (traverseNode != null)
+            traverseNode = traverseNode.next;
+        if (reverseNode != null)
+            reverseNode.next = null;
+
+        while (traverseNode != null) {
+            tempNode = new ListNode(traverseNode.val);
+            tempNode.next = reverseNode;
+            reverseNode = tempNode;
+            traverseNode = traverseNode.next;
+        }
+        return reverseNode;
     }
 }
--- src/test/java/com/leetcode/algorithm/easy/ReverseListTest.java
+++ src/test/java/com/leetcode/algorithm/easy/ReverseListTest.java
@@ -1,11 +1,14 @@
 package com.leetcode.algorithm.easy;
 
 import com.leetcode.algorithm.common.struct.LinkedList;
+import com.leetcode.algorithm.common.struct.ListNode;
 import com.leetcode.algorithm.common.util.LinkedListUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class ReverseListTest {
@@ -15,8 +18,11 @@ public class ReverseListTest {
         LinkedList linkedList = LinkedListUtil.generateLinkedListSample(Arrays.asList(1, 2, 3, 4, 5));
         linkedList.printList();
 
-        new ReverseList().reverseList(linkedList.head);
-//        assertThat()
-
+        ListNode tempNode = new ReverseList().reverseList(linkedList.head);
+        int count = 5;
+        while (tempNode != null) {
+            assertThat(tempNode.val).isEqualTo(count--);
+            tempNode = tempNode.next;
+        }
     }
 }
