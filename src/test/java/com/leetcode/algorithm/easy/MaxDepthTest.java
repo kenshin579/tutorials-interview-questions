ba0c5ef880a6eee71c3a775d0b7af25c46e0ba54
frankyoh <frankyoh@tmon.co.kr>
2020-03-07 17:38:10 +0900

    leetcode MaxDepthTest 수정

--- src/test/java/com/leetcode/algorithm/easy/MaxDepthTest.java
+++ src/test/java/com/leetcode/algorithm/easy/MaxDepthTest.java
@@ -1,7 +1,7 @@
 package com.leetcode.algorithm.easy;
 
-import com.leetcode.algorithm.common.struct.BinarySearchTree;
-import com.leetcode.algorithm.common.util.BinarySearchTreeUtil;
+import com.leetcode.algorithm.common.struct.BinaryTree;
+import com.leetcode.algorithm.common.util.BinaryTreeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -14,25 +14,25 @@ public class MaxDepthTest {
 
     @Test
     public void maxDepth1() {
-        BinarySearchTree binarySearchTree = BinarySearchTreeUtil.generateBinaryTree(Arrays.asList(5, 2, 7, 8, 6));
-        binarySearchTree.print();
+        BinaryTree binaryTree = BinaryTreeUtil.generateBinaryTree(Arrays.asList(3, 9, 20, null, null, 15, 7));
+        binaryTree.print();
 
-        assertThat(new MaxDepth().maxDepth(binarySearchTree.root)).isEqualTo(3);
+        assertThat(new MaxDepth().maxDepth(binaryTree.root)).isEqualTo(3);
     }
 
     @Test
     public void maxDepth2() {
-        BinarySearchTree binarySearchTree = BinarySearchTreeUtil.generateBinaryTree(Arrays.asList());
-        binarySearchTree.print();
+        BinaryTree binaryTree = BinaryTreeUtil.generateBinaryTree(Arrays.asList());
+        binaryTree.print();
 
-        assertThat(new MaxDepth().maxDepth(binarySearchTree.root)).isEqualTo(0);
+        assertThat(new MaxDepth().maxDepth(binaryTree.root)).isEqualTo(0);
     }
 
     @Test
     public void maxDepth3() {
-        BinarySearchTree binarySearchTree = BinarySearchTreeUtil.generateBinaryTree(Arrays.asList(5, 2));
-        binarySearchTree.print();
+        BinaryTree binaryTree = BinaryTreeUtil.generateBinaryTree(Arrays.asList(5, 2));
+        binaryTree.print();
 
-        assertThat(new MaxDepth().maxDepth(binarySearchTree.root)).isEqualTo(2);
+        assertThat(new MaxDepth().maxDepth(binaryTree.root)).isEqualTo(2);
     }
 }
