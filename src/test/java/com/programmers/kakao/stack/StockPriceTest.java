1c4650305e68f36079bad622eb6b2d2a72f3f138
frankyoh <frankyoh@tmon.co.kr>
2020-07-16 00:03:39 +0900

    주식가격 완료

--- src/test/java/com/programmers/kakao/stack/StockPriceTest.java
+++ src/test/java/com/programmers/kakao/stack/StockPriceTest.java
@@ -7,14 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StockPriceTest {
 
     /**
-     * 반환값 :가격이 떨어지지 않은 기간은 몇 초 카운트하면 됨
+     * 반환값 : 가격이 떨어지지 않은 기간은 몇 초 카운트하면 됨
      */
     @Test
     public void solution() {
         int[] prices = {1, 2, 3, 2, 3};
         int[] answer = {4, 3, 1, 1, 0};
 
-        assertThat(new StockPrice().solution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().bestSolution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().mySolution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().solution2(prices)).isEqualTo(answer);
     }
 
     @Test
@@ -22,6 +24,18 @@ public class StockPriceTest {
         int[] prices = {1, 1, 1};
         int[] answer = {2, 1, 0};
 
-        assertThat(new StockPrice().solution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().bestSolution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().mySolution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().solution2(prices)).isEqualTo(answer);
+    }
+
+    @Test
+    public void solution3() {
+        int[] prices = {2, 3, 6, 7, 5, 3, 5, 7, 8, 3, 2};
+        int[] answer = {10, 9, 2, 1, 1, 5, 3, 2, 1, 1, 0};
+
+        assertThat(new StockPrice().bestSolution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().mySolution(prices)).isEqualTo(answer);
+        assertThat(new StockPrice().solution2(prices)).isEqualTo(answer);
     }
 }
