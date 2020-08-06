d3e4f2e8952edee38938403998e28433b8c86132
frankyoh <frankyoh@tmon.co.kr>
2020-08-06 21:04:52 +0900

    [leetcode] PowerOfFour 스터디 완료

--- src/main/java/com/leetcode/algorithm/easy/PowerOfFour.java
+++ src/main/java/com/leetcode/algorithm/easy/PowerOfFour.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 342. Power of Four
  * https://leetcode.com/problems/power-of-four/
+ * https://www.geeksforgeeks.org/find-whether-a-given-number-is-a-power-of-4-or-not/
  */
 @Slf4j
 public class PowerOfFour {
@@ -14,6 +15,7 @@ public class PowerOfFour {
      * Algorithm :
      * - 4를 곱하면서 기존 num와 같은 같이 나오면 true 아니면 false를 반환함
      * Issue : Time Limit Exceeded 발생함
+     * 단점 : negative이면 무한으로 늘어남
      */
     public boolean isPowerOfFour(int num) {
         int number = 1;
@@ -27,13 +29,45 @@ public class PowerOfFour {
     }
 
     /**
-     * Loops/Recursion 사용하지 않고 하는 방법?
      * Time Complexity :
      * <p>
      * Algorithm :
+     * - loop을 돌면서 N % 4의 값이 0이 아니면 false
      */
     public boolean isPowerOfFour2(int num) {
-        log.info("num : {} {}", num, Integer.toBinaryString(num));
+        while (num != 1) {
+            log.info("num : {} {}", num, num % 4);
+            if (num % 4 != 0) {
+                return false;
+            }
+            num /= 4;
+        }
+        return true;
+    }
+
+    /**
+     * Time Complexity : O(B)
+     * B : bit 갯수만큼
+     * <p>
+     * Algorithm :
+     * - first bit이 1일지 확인
+     * - zero bit의 수가 even이면
+     */
+    public boolean isPowerOfFour3(int num) {
+        if (num == 1) return true;
+        int count = 0;
+        int firstBitCheck = num & (num - 1);
+        if (firstBitCheck == 0 && num > 0) {
+            //zero bit check
+            while (num != 1) {
+                log.info("num : {} count : {}", num, count);
+                num >>= 1;
+//                count *= 2;
+                count++;
+            }
+            return count % 2 == 0;
+        }
         return false;
+
     }
 }
--- src/test/java/com/leetcode/algorithm/easy/PowerOfFourTest.java
+++ src/test/java/com/leetcode/algorithm/easy/PowerOfFourTest.java
@@ -12,36 +12,63 @@ public class PowerOfFourTest {
     @Test
     public void isPowerOfFour() {
         assertThat(new PowerOfFour().isPowerOfFour(16)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour2(16)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour3(16)).isTrue();
     }
 
     @Test
     public void isPowerOfFour2() {
         assertThat(new PowerOfFour().isPowerOfFour(5)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour2(5)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour3(5)).isFalse();
     }
 
     @Test
     public void isPowerOfFour3() {
         assertThat(new PowerOfFour().isPowerOfFour(4)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour2(4)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour3(4)).isTrue();
     }
 
     @Test
     public void isPowerOfFour4() {
         assertThat(new PowerOfFour().isPowerOfFour(256)).isTrue();
-//        assertThat(new PowerOfFour().isPowerOfFour2(256)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour2(256)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour3(256)).isTrue();
     }
 
     @Test
     public void isPowerOfFour5() {
         assertThat(new PowerOfFour().isPowerOfFour(1024)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour2(1024)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour3(1024)).isTrue();
     }
 
     @Test
     public void isPowerOfFour6() {
         assertThat(new PowerOfFour().isPowerOfFour(81)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour2(81)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour3(81)).isFalse();
     }
 
     @Test
     public void isPowerOfFour7() {
         assertThat(new PowerOfFour().isPowerOfFour(1)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour2(1)).isTrue();
+        assertThat(new PowerOfFour().isPowerOfFour3(1)).isTrue();
+    }
+
+    @Test
+    public void isPowerOfFour8() {
+        assertThat(new PowerOfFour().isPowerOfFour(-2147483648)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour2(-2147483648)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour3(-2147483648)).isFalse();
+    }
+
+    @Test
+    public void isPowerOfFour9() {
+        assertThat(new PowerOfFour().isPowerOfFour(2)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour2(2)).isFalse();
+        assertThat(new PowerOfFour().isPowerOfFour3(2)).isFalse();
     }
 }
