8c2d65d72087450263d85618b15fa08cbaf2c452
frankyoh <frankyoh@tmon.co.kr>
2020-08-10 21:21:12 +0900

    [leetcode] WordDictionary 스터디중

---
+++ src/main/java/com/leetcode/algorithm/medium/WordDictionary.java
@@ -0,0 +1,43 @@
+package com.leetcode.algorithm.medium;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * 211. Add and Search Word - Data structure design
+ * https://leetcode.com/problems/add-and-search-word-data-structure-design/
+ * Time Limit Exceeded 발생함
+ */
+@Slf4j
+public class WordDictionary {
+    private Set<String> dict;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public WordDictionary() {
+        dict = new HashSet<>();
+    }
+
+    /**
+     * Adds a word into the data structure.
+     */
+    public void addWord(String word) {
+        dict.add(word);
+    }
+
+    /**
+     * Returns if the word is in the data structure.
+     * A word could contain the dot character '.' to represent any one letter.
+     */
+    public boolean search(String word) {
+        if (word.contains(".")) {
+            return dict.stream().anyMatch(it -> Pattern.matches(word, it));
+        } else {
+            return dict.contains(word);
+        }
+    }
+}
