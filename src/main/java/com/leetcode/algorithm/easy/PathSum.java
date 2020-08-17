caeb35453f1bbc6193f9a734a7940242d7613d16
frankyoh <frankyoh@tmon.co.kr>
2020-08-17 17:22:08 +0900

    [ALG-32] leetcode : Path Sum 스터디중

--- src/main/java/com/leetcode/algorithm/easy/PathSum.java
+++ src/main/java/com/leetcode/algorithm/easy/PathSum.java
@@ -3,10 +3,25 @@ package com.leetcode.algorithm.easy;
 import com.leetcode.algorithm.common.struct.TreeNode;
 
 /**
- * https://leetcode.com/problems/path-sum-iii/
+ * 112. Path Sum
+ * https://leetcode.com/problems/path-sum/
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 public class PathSum {
-    public int pathSum(TreeNode root, int sum) {
-        return 0;
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return false;
     }
 }
