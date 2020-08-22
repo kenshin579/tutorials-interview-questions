9237a4aeecddc77602ea6c5d82979198ae1812c5
frankyoh <frankyoh@tmon.co.kr>
2020-08-22 15:52:44 +0900

    [ALG-33] leetcode : SortedListToBST 스터디중

---
+++ src/main/java/com/leetcode/algorithm/medium/SortedListToBST.java
@@ -0,0 +1,39 @@
+package com.leetcode.algorithm.medium;
+
+/**
+ * 109. Convert Sorted List to Binary Search Tree
+ * https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+ * <p>
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+import com.leetcode.algorithm.common.struct.ListNode;
+import com.leetcode.algorithm.common.struct.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class SortedListToBST {
+    public TreeNode sortedListToBST(ListNode head) {
+        return null;
+    }
+}
