c2efbcc32f0d278c304e84276efa5a75bfb97113
frankyoh <frankyoh@tmon.co.kr>
2020-08-25 20:24:30 +0900

    [ALG-43] SumOfLeftLeaves 스터디중

--- src/main/java/com/leetcode/algorithm/easy/SumOfLeftLeaves.java
+++ src/main/java/com/leetcode/algorithm/easy/SumOfLeftLeaves.java
@@ -3,6 +3,9 @@ package com.leetcode.algorithm.easy;
 import com.leetcode.algorithm.common.struct.TreeNode;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 404. Sum of Left Leaves
  * https://leetcode.com/problems/sum-of-left-leaves/
@@ -23,7 +26,28 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class SumOfLeftLeaves {
+    /**
+     * Time Complexity :
+     *
+     * Algorithm :
+     */
     public int sumOfLeftLeaves(TreeNode root) {
-        return 0;
+        int sum = 0;
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        TreeNode current;
+
+        while(!stack.isEmpty()) {
+            current = stack.pop();
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+
+        return sum;
     }
 }
