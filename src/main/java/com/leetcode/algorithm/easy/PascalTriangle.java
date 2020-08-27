7592d53adcf93599604184305d88da51e58eaed5
frankyoh <frankyoh@tmon.co.kr>
2020-08-27 22:22:15 +0900

    [ALG-50] PascalTriangle 스터디완료

--- src/main/java/com/leetcode/algorithm/easy/PascalTriangle.java
+++ src/main/java/com/leetcode/algorithm/easy/PascalTriangle.java
@@ -1,19 +1,44 @@
 package com.leetcode.algorithm.easy;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 118. Pascal's Triangle
  * https://leetcode.com/problems/pascals-triangle/
  */
+@Slf4j
 public class PascalTriangle {
     /**
-     * Time Complexity :
-     *
-     * Algoritym :
+     * Time Complexity : O(N^2)
+     * <p>
+     * Algorithm :
+     * 1. list of list를 만들어야 해서 2개의 loop안에서
+     * - base case : [[1], [1, 1]]를 만들고
+     * 2. numRows 3부터는 중간 값은 중간값을 더해서 계산한 값을 list에 추가함
      */
     public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> sumList;
 
-        return null;
+        for (int i = 0; i < numRows; i++) {
+            sumList = new ArrayList<>();
+            log.info("i:{}", i);
+            for (int j = 0; j <= i; j++) {
+                log.info("j:{} {}", j, sumList);
+                if (j == 0 || j == i) {
+                    sumList.add(1);
+                } else {
+                    sumList.add(result.get(i - 1).get(j)
+                            + result.get(i - 1).get(j - 1));
+                }
+            }
+            result.add(sumList);
+            log.info("result : {}", result);
+            System.out.println();
+        }
+        return result;
     }
 }
--- src/test/java/com/leetcode/algorithm/easy/PascalTriangleTest.java
+++ src/test/java/com/leetcode/algorithm/easy/PascalTriangleTest.java
@@ -7,22 +7,56 @@ import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class PascalTriangleTest {
 
     @Test
     public void generate() {
-        List<List<Integer>> result = Arrays.asList(
+        List<List<Integer>> expected = Arrays.asList(
                 asList(1),
-                asList(1,1),
-                asList(1,2,1),
-                asList(1,3,1,1),
-                asList(1,4,6,4,1)
+                asList(1, 1),
+                asList(1, 2, 1),
+                asList(1, 3, 3, 1),
+                asList(1, 4, 6, 4, 1)
         );
 
         List<List<Integer>> generate = new PascalTriangle().generate(5);
-        log.info("generate : {}", generate);
+        assertThat(generate).isEqualTo(expected);
 
+    }
+
+    @Test
+    public void generate2() {
+        List<List<Integer>> expected = Arrays.asList(
+                asList(1)
+        );
+
+        List<List<Integer>> generate = new PascalTriangle().generate(1);
+        assertThat(generate).isEqualTo(expected);
+    }
+
+    @Test
+    public void generate3() {
+        List<List<Integer>> expected = Arrays.asList(
+                asList(1),
+                asList(1, 1)
+        );
+
+        List<List<Integer>> generate = new PascalTriangle().generate(2);
+        assertThat(generate).isEqualTo(expected);
+    }
+
+    @Test
+    public void generate4() {
+        List<List<Integer>> expected = Arrays.asList(
+                asList(1),
+                asList(1, 1),
+                asList(1, 2, 1)
+        );
+
+        List<List<Integer>> generate = new PascalTriangle().generate(3);
+        assertThat(generate).isEqualTo(expected);
     }
 }
