7f86db30befd787c205e86126457f49dd4ac826e
kenshin579 <kenshin579@hotmail.com>
2020-09-02 22:56:47 +0900

    [ALG-63] MergeTwoLists init

---
+++ src/main/java/com/leetcode/algorithm/easy/MergeTwoLists.java
@@ -0,0 +1,30 @@
+package com.leetcode.algorithm.easy;
+
+import com.leetcode.algorithm.common.struct.ListNode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 21. Merge Two Sorted Lists
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ */
+@Slf4j
+public class MergeTwoLists {
+    /**
+     * Time Complexity :
+     *
+     * Algorithm :
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        ListNode newNode = new ListNode();
+
+        return null;
+    }
+}
---
+++ src/test/java/com/leetcode/algorithm/easy/MergeTwoListsTest.java
@@ -0,0 +1,12 @@
+package com.leetcode.algorithm.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeTwoListsTest {
+
+    @Test
+    public void mergeTwoLists() {
+    }
+}
