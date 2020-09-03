07d6b20e0c16c8b6685cf94d2082b9f33b51cc66
kenshin579 <kenshin579@hotmail.com>
2020-09-03 22:24:41 +0900

    [ALG-61] ListNode 객체 수정

--- src/main/java/com/leetcode/algorithm/common/struct/ListNode.java
+++ src/main/java/com/leetcode/algorithm/common/struct/ListNode.java
@@ -1,10 +1,18 @@
 package com.leetcode.algorithm.common.struct;
 
+import lombok.ToString;
+
+@ToString
 public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode(int x) {
-        val = x;
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
