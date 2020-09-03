fc59e7a9e87d107f0f075ce8e4ba35086da06f29
kenshin579 <kenshin579@hotmail.com>
2020-09-03 22:28:26 +0900

    [ALG-63] MergeTwoLists 스터디중

--- src/test/java/com/leetcode/algorithm/easy/MergeTwoListsTest.java
+++ src/test/java/com/leetcode/algorithm/easy/MergeTwoListsTest.java
@@ -1,12 +1,20 @@
 package com.leetcode.algorithm.easy;
 
+import com.leetcode.algorithm.common.struct.ListNode;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class MergeTwoListsTest {
 
     @Test
     public void mergeTwoLists() {
+        ListNode l1 = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(4)));
+
+        ListNode l2 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(4)));
+
+        ListNode result = new MergeTwoLists().mergeTwoLists(l1, l2);
     }
 }
