7fcf28941feab907388ae680c52c4d7471f6b7d8
kenshin579 <kenshin579@hotmail.com>
2020-09-10 18:08:48 +0900

    [ALG-61] InsertionSortList 스터디중

--- src/main/java/com/leetcode/algorithm/medium/InsertionSortList.java
+++ src/main/java/com/leetcode/algorithm/medium/InsertionSortList.java
@@ -6,26 +6,41 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 147. Insertion Sort List
  * https://leetcode.com/problems/insertion-sort-list/
- *
+ * <p>
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *
+ * https://leetcode.com/problems/insertion-sort-list/discuss/812288/Clean-Java-solution-EASY-to-understand
  * }
  */
 @Slf4j
 public class InsertionSortList {
     /**
+     * todo : 이해가 잘 안됨
      * Time Complexity :
-     *
+     * <p>
      * Algorithm :
-     *
      */
     public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(-1);
 
-        return null;
+        while (head != null) {
+            ListNode node = dummy;
+            while (node.next != null && node.next.val < head.val) {
+                node = node.next;
+            }
+
+            ListNode temp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = temp;
+        }
+
+        return dummy.next;
     }
 }
--- src/test/java/com/leetcode/algorithm/medium/InsertionSortListTest.java
+++ src/test/java/com/leetcode/algorithm/medium/InsertionSortListTest.java
@@ -5,7 +5,10 @@ import com.leetcode.algorithm.common.util.ListNodeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class InsertionSortListTest {
@@ -22,5 +25,38 @@ public class InsertionSortListTest {
         ListNode result = new InsertionSortList().insertionSortList(head);
 
         ListNodeUtil.print(result);
+    }
+
+    @Test
+    public void insertionSortList2() {
+        ListNode head = new ListNode(4);
+        ListNodeUtil.print(head);
+
+        ListNode result = new InsertionSortList().insertionSortList(head);
+        ListNodeUtil.print(result);
+        assertListNode(result);
+    }
+
+    @Test
+    public void insertionSortList3() {
+        ListNode head = new ListNode(4,
+                new ListNode(2));
+        ListNodeUtil.print(head);
+
+        ListNode result = new InsertionSortList().insertionSortList(head);
+        ListNodeUtil.print(result);
+        assertListNode(result);
+    }
+
+    private void assertListNode(ListNode head) {
+        List<Integer> result = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            result.add(current.val);
+            current = current.next;
+        }
+
+        assertThat(result).isNotEmpty();
+        assertThat(result).isSorted();
     }
 }
