667407a6e17e3efbd2b9492ff68aed1e4638746a
kenshin579 <kenshin579@hotmail.com>
2020-09-10 18:13:31 +0900

    [ALG-66] DeleteNode init

---
+++ src/main/java/com/leetcode/algorithm/medium/DeleteNode.java
@@ -0,0 +1,26 @@
+package com.leetcode.algorithm.medium;
+
+import com.leetcode.algorithm.common.struct.TreeNode;
+
+/**
+ * 450. Delete Node in a BST
+ * https://leetcode.com/problems/delete-node-in-a-bst/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class DeleteNode {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return null;
+    }
+}
