a85532901d18bc313e76c3ec4951d822816e7b91
kenshin579 <kenshin579@hotmail.com>
2020-09-12 17:33:38 +0900

    [ALG-63] MergeTwoLists 스터디완료

--- src/main/java/com/leetcode/algorithm/easy/MergeTwoLists.java
+++ src/main/java/com/leetcode/algorithm/easy/MergeTwoLists.java
@@ -6,31 +6,81 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 21. Merge Two Sorted Lists
  * https://leetcode.com/problems/merge-two-sorted-lists/
- *
+ * <p>
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  */
 @Slf4j
 public class MergeTwoLists {
     /**
-     * Time Complexity :
-     *
+     * Time Complexity : O(N+M)
+     * <p>
      * Algorithm :
+     * 1. l1, l2의 값을 비교해서 result listNode에 하나씩 추가한다
+     * 2. l1, l2 남아 있는 부분을 추가하도록 함
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode result;
+        ListNode current = null;
+        ListNode result = null;
 
         while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                result = new ListNode(l1.val);
-
+            log.info("l1.val:{}, l2.val:{}", l1.val, l2.val);
+            if (l1.val <= l2.val) {
+                log.info("case1");
+                if (current == null) {
+                    current = new ListNode(l1.val);
+                    result = current;
+                } else {
+                    current.next = new ListNode(l1.val);
+                    current = current.next;
+                }
+                l1 = l1.next;
+            } else {
+                log.info("case2");
+                if (current == null) {
+                    current = new ListNode(l2.val);
+                    result = current;
+                } else {
+                    current.next = new ListNode(l2.val);
+                    current = current.next;
+                }
+                l2 = l2.next;
             }
+            if (result == null) {
+                result = current;
+            }
+            log.info("result : {}", result);
+            System.out.println();
+
         }
-        return null;
+
+        while (l1 != null) {
+            if (current == null) {
+                current = new ListNode(l1.val);
+                result = current;
+            } else {
+                current.next = new ListNode(l1.val);
+                current = current.next;
+            }
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            if (current == null) {
+                current = new ListNode(l2.val);
+                result = current;
+            } else {
+                current.next = new ListNode(l2.val);
+                current = current.next;
+            }
+            l2 = l2.next;
+        }
+
+        return result;
     }
 }
--- src/test/java/com/leetcode/algorithm/easy/MergeTwoListsTest.java
+++ src/test/java/com/leetcode/algorithm/easy/MergeTwoListsTest.java
@@ -21,13 +21,73 @@ public class MergeTwoListsTest {
                         new ListNode(4)));
 
         ListNode result = new MergeTwoLists().mergeTwoLists(l1, l2);
-        assertListNode(result);
+        assertListNode(result, 6);
 
     }
 
-    private void assertListNode(ListNode head) {
+    @Test
+    public void mergeTwoLists2() {
+        ListNode l1 = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(4)));
+
+        ListNode l2 = new ListNode(1,
+                new ListNode(3));
+
+        ListNode result = new MergeTwoLists().mergeTwoLists(l1, l2);
+        assertListNode(result, 5);
+    }
+
+    @Test
+    public void mergeTwoLists3() {
+        ListNode l1 = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(4)));
+
+        ListNode l2 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(4,
+                                new ListNode(6))));
+
+        ListNode result = new MergeTwoLists().mergeTwoLists(l1, l2);
+        assertListNode(result, 7);
+    }
+
+    @Test
+    public void mergeTwoLists4() {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+
+        ListNode result = new MergeTwoLists().mergeTwoLists(l1, l2);
+        assertListNode(result, 2);
+    }
+
+    @Test
+    public void mergeTwoLists5() {
+        ListNode l1 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(4,
+                                new ListNode(6))));
+
+        ListNode result = new MergeTwoLists().mergeTwoLists(l1, null);
+        assertListNode(result, 4);
+    }
+
+    @Test
+    public void mergeTwoLists6() {
+        ListNode l2 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(4,
+                                new ListNode(6))));
+
+        ListNode result = new MergeTwoLists().mergeTwoLists(null, l2);
+        assertListNode(result, 4);
+    }
+
+    private void assertListNode(ListNode head, int totalSize) {
         List<Integer> intValues = ListNodeUtil.convertToList(head);
 
+        assertThat(intValues.size()).isEqualTo(totalSize);
         assertThat(intValues).isNotEmpty();
         assertThat(intValues).isSorted();
     }
