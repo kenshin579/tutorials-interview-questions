a7291e3df4f27cf7c43b0be2a0c38ef41141fa8f
kenshin579 <kenshin579@hotmail.com>
2020-09-15 23:13:45 +0900

    [ALG-78] reverseList unit test 수정

--- src/test/java/com/leetcode/algorithm/easy/ReverseListTest.java
+++ src/test/java/com/leetcode/algorithm/easy/ReverseListTest.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,11 +20,20 @@ public class ReverseListTest {
         LinkedList linkedList = LinkedListUtil.convertLinkedList(Arrays.asList(1, 2, 3, 4, 5));
         ListNodeUtil.print(linkedList.head);
 
-        ListNode tempNode = new ReverseList().reverseList(linkedList.head);
-        int count = 5;
-        while (tempNode != null) {
-            assertThat(tempNode.val).isEqualTo(count--);
-            tempNode = tempNode.next;
-        }
+        ListNode result = new ReverseList().reverseList(linkedList.head);
+        ListNodeUtil.print(result);
+        List<Integer> values = ListNodeUtil.convertToList(result);
+        assertThat(values).isEqualTo(Arrays.asList(5, 4, 3, 2, 1));
+    }
+
+    @Test
+    public void reverseList2() {
+        LinkedList linkedList = LinkedListUtil.convertLinkedList(Arrays.asList(1, 2));
+        ListNodeUtil.print(linkedList.head);
+
+        ListNode result = new ReverseList().reverseList(linkedList.head);
+        ListNodeUtil.print(result);
+        List<Integer> values = ListNodeUtil.convertToList(result);
+        assertThat(values).isEqualTo(Arrays.asList(2, 1));
     }
 }
