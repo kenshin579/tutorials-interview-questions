6f97cdccf71da6da699796874a64a2550abe69c1
kenshin579 <kenshin579@hotmail.com>
2020-09-17 19:55:53 +0900

    [ALG-71] ReverseBetween 스터디중

--- src/main/java/com/leetcode/algorithm/medium/ReverseBetween.java
+++ src/main/java/com/leetcode/algorithm/medium/ReverseBetween.java
@@ -1,27 +1,68 @@
 package com.leetcode.algorithm.medium;
 
 import com.leetcode.algorithm.common.struct.ListNode;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 92. Reverse Linked List II
  * https://leetcode.com/problems/reverse-linked-list-ii/
- *
+ * <p>
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+@Slf4j
 public class ReverseBetween {
     /**
-     * Time Complexity :
-     *
+     * Time Complexity : O(N)
+     * <p>
      * Algorithm :
      */
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        return null;
+        int index = 1;
+        ListNode prevNode = null;
+        ListNode currNode = head;
+        ListNode nextNode;
+        ListNode frontNode = null;
+        ListNode endNode = null;
+        ListNode tailNode = null;
+
+        if (m == n) {
+            return head;
+        }
+
+        while (currNode != null) {
+            log.info("index : {} currNode.val: {}", index, currNode.val);
+            if (index < m) {
+                if (index == m - 1) {
+                    frontNode = currNode;
+                }
+                nextNode = currNode.next; //c의 다음 노드를 가리킴
+                currNode = nextNode; //c(현재) -> n(다음)을 가리키도록 함
+            } else if (index > n) {
+                endNode = currNode;
+                break;
+            } else {
+                nextNode = currNode.next; //c의 다음 노드를 가리킴
+                currNode.next = prevNode; //c -> p를 reverse하게 가리키도록 함
+                if (prevNode == null) {
+                    tailNode = currNode;
+                }
+                //다음 step을 위해서
+                prevNode = currNode; //p(전노드) -> c(현재)를 가리키도록 함
+                currNode = nextNode; //c(현재) -> n(다음)을 가리키도록 함
+            }
+            index++;
+        }
+
+        //front -> reverse -> end 연결하기
+        frontNode.next = prevNode;
+        tailNode.next = endNode;
+        return head;
     }
 }
--- src/test/java/com/leetcode/algorithm/medium/ReverseBetweenTest.java
+++ src/test/java/com/leetcode/algorithm/medium/ReverseBetweenTest.java
@@ -23,4 +23,26 @@ public class ReverseBetweenTest {
         List<Integer> values = ListNodeUtil.convertToList(result);
         assertThat(values).isEqualTo(Arrays.asList(1, 4, 3, 2, 5));
     }
+
+    @Test
+    public void reverseBetween2() {
+        LinkedList linkedList = LinkedListUtil.convertLinkedList(Arrays.asList(5));
+        ListNodeUtil.print(linkedList.head);
+
+        ListNode result = new ReverseBetween().reverseBetween(linkedList.head, 1, 1);
+        ListNodeUtil.print(result);
+        List<Integer> values = ListNodeUtil.convertToList(result);
+        assertThat(values).isEqualTo(Arrays.asList(5));
+    }
+
+    @Test
+    public void reverseBetween3() {
+        LinkedList linkedList = LinkedListUtil.convertLinkedList(Arrays.asList(3, 5));
+        ListNodeUtil.print(linkedList.head);
+
+        ListNode result = new ReverseBetween().reverseBetween(linkedList.head, 1, 2);
+        ListNodeUtil.print(result);
+        List<Integer> values = ListNodeUtil.convertToList(result);
+        assertThat(values).isEqualTo(Arrays.asList(5, 3));
+    }
 }
