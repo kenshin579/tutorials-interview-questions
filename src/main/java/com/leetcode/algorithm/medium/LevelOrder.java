a81c48be53159db3d1e8bb02b12f0bd94b009285
kenshin579 <kenshin579@hotmail.com>
2020-09-21 20:36:52 +0900

    [ALG-87] LevelOrder init

---
+++ src/main/java/com/leetcode/algorithm/medium/LevelOrder.java
@@ -0,0 +1,20 @@
+package com.leetcode.algorithm.medium;
+
+import com.leetcode.algorithm.common.struct.TreeNode;
+
+import java.util.List;
+
+/**
+ * 102. Binary Tree Level Order Traversal
+ * Difficulty : Medium
+ * Tags : Tree
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ *
+ */
+@Deprecated
+public class LevelOrder {
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		return null;
+	}
+}
