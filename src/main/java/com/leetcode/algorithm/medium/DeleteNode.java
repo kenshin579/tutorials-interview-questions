4952b2388ccd1eab33e5119728c1d046af93e0e1
kenshin579 <kenshin579@hotmail.com>
2020-09-21 20:31:09 +0900

    [ALG-66] DeleteNode 스터디중

--- src/main/java/com/leetcode/algorithm/medium/DeleteNode.java
+++ src/main/java/com/leetcode/algorithm/medium/DeleteNode.java
@@ -4,6 +4,9 @@ import com.leetcode.algorithm.common.struct.TreeNode;
 
 /**
  * 450. Delete Node in a BST
+ * Difficulty : Medium
+ * Tags : Tree
+ *
  * https://leetcode.com/problems/delete-node-in-a-bst/
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -19,8 +22,25 @@ import com.leetcode.algorithm.common.struct.TreeNode;
  *     }
  * }
  */
+@Deprecated
 public class DeleteNode {
+    /**
+     * Time Complexity :
+     * Algorithm : 3가지 cases
+     * Case 1: leaf 노드
+     * - 그냥 삭제하면 됨
+     *
+     * Case 2: child 노드가 하나 있는 경우
+     * - child 노드를 삭제한 곳에 넣으면 됨
+     *
+     * Case 3: child 노드가 2개인 경우
+     * - 삭제되는 노드의 inorder successor를 찾아서 삭제된 노드에 넣으면 됨
+     *
+     * https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
+     * https://www.techiedelight.com/deletion-from-bst/
+     */
     public TreeNode deleteNode(TreeNode root, int key) {
-        return null;
+
+        return root;
     }
 }
--- src/test/java/com/leetcode/algorithm/medium/DeleteNodeTest.java
+++ src/test/java/com/leetcode/algorithm/medium/DeleteNodeTest.java
@@ -1,10 +1,38 @@
 package com.leetcode.algorithm.medium;
 
+import com.leetcode.algorithm.common.struct.BinarySearchTree;
+import com.leetcode.algorithm.common.struct.TreeNode;
+import com.leetcode.algorithm.common.util.BTreePrinter;
+import com.leetcode.algorithm.common.util.BinarySearchTreeUtil;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class DeleteNodeTest {
 
-    @Test
-    public void deleteNode() {
-    }
+	@Test
+	public void deleteNode() {
+		BinarySearchTree binarySearchTree = BinarySearchTreeUtil.generateBinaryTree(Arrays.asList(5, 3, 6, 2, 4, 7));
+		binarySearchTree.print();
+
+		TreeNode result = new DeleteNode().deleteNode(binarySearchTree.root, 3);
+		BTreePrinter.printNode(result);
+
+		//        assertThat(result)
+
+	}
+
+	@Test
+	public void deleteNode2() {
+		BinarySearchTree binarySearchTree = BinarySearchTreeUtil.generateBinaryTree(Arrays.asList());
+		binarySearchTree.print();
+
+		TreeNode result = new DeleteNode().deleteNode(binarySearchTree.root, 3);
+		BTreePrinter.printNode(result);
+
+		//        assertThat(result)
+
+	}
+
+
 }
