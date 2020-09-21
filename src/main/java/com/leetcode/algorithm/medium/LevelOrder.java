298bc89041d61a33689954af55327fbc770c818f
kenshin579 <kenshin579@hotmail.com>
2020-09-21 21:05:34 +0900

    [ALG-87] LevelOrder 스터디중

--- src/main/java/com/leetcode/algorithm/medium/LevelOrder.java
+++ src/main/java/com/leetcode/algorithm/medium/LevelOrder.java
@@ -2,19 +2,60 @@ package com.leetcode.algorithm.medium;
 
 import com.leetcode.algorithm.common.struct.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 102. Binary Tree Level Order Traversal
  * Difficulty : Medium
  * Tags : Tree
- *
+ * <p>
  * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * <p>
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  *
+ * }
  */
 @Deprecated
 public class LevelOrder {
 	public List<List<Integer>> levelOrder(TreeNode root) {
+		Queue<TreeNode> queue = new ArrayDeque<>();
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> subResult;
+
+		TreeNode current;
+		if (root != null) {
+			queue.add(root);
+		}
+
+		while (!queue.isEmpty()) {
+			current = queue.poll();
+			subResult = new ArrayList<>();
+
+			subResult.add(current.val);
+
+			subResult = new ArrayList<>();
+
+			if (current.left != null) {
+				queue.add(current.left);
+			}
+			if (current.right != null) {
+				queue.add(current.right);
+			}
+		}
 		return null;
 	}
 }
--- src/test/java/com/leetcode/algorithm/medium/LevelOrderTest.java
+++ src/test/java/com/leetcode/algorithm/medium/LevelOrderTest.java
@@ -1,17 +1,49 @@
 package com.leetcode.algorithm.medium;
 
-import com.leetcode.algorithm.common.struct.BinaryTree;
-import com.leetcode.algorithm.common.util.BinaryTreeUtil;
+import com.leetcode.algorithm.common.struct.TreeNode;
 import org.junit.Test;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LevelOrderTest {
 
 	@Test
 	public void levelOrder() {
 		//todo : 다시 보기
-		BinaryTree binaryTree = BinaryTreeUtil.generateBinaryTree(Arrays.asList(3, 9, 20, null, null, 15, 7));
-		binaryTree.print();
+		//		BinaryTree binaryTree = BinaryTreeUtil.generateBinaryTree(Arrays.asList(3, 9, 20, null, null, 15, 7));
+		//		binaryTree.print();
+		TreeNode root = new TreeNode(
+				3,
+				new TreeNode(9),
+				new TreeNode(20,
+						new TreeNode(15),
+						new TreeNode(7))
+
+		);
+
+		List<List<Integer>> expected = Arrays.asList(
+				asList(3),
+				asList(9, 20),
+				asList(15, 7)
+		);
+
+		List<List<Integer>> result = new LevelOrder().levelOrder(root);
+		assertThat(result).isEqualTo(expected);
+	}
+
+	@Test
+	public void ArrayDequeTest() {
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(1);
+		queue.add(3);
+		queue.add(10);
+		assertThat(queue.poll()).isEqualTo(1);
+		assertThat(queue.poll()).isEqualTo(3);
 	}
 }
