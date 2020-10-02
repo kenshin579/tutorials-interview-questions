7459ad889306307d47743e70a7f1db2e345c97fb
rocketFuel <50790681+dinitrogen-tetroxide@users.noreply.github.com>
2020-10-02 15:19:02 +1000

    Created TreeSame.java

---
+++ src/main/java/com/leetcode/algorithm/easy/SameTree.java
@@ -0,0 +1,19 @@
+package com.leetcode.algorithm.easy;
+
+/**
+ * https://leetcode.com/problems/same-tree/
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        else if ((p != null && q !=null) && p.val == q.val) {
+            boolean left = isSameTree(p.left, q.left);
+            boolean right = isSameTree(p.right, q.right);
+            
+            return left && right;
+        }
+        return false;
+    }
+}
