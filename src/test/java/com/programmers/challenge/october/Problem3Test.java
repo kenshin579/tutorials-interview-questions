be37e09f0428b602f870aaa41022c911cec37d5a
kenshin579 <kenshin579@hotmail.com>
2020-10-08 22:15:49 +0900

    [ALG-117] 프로그래밍3 스터디중 - 그래프에 대한 스터디가 필요함

---
+++ src/test/java/com/programmers/challenge/october/Problem3Test.java
@@ -0,0 +1,30 @@
+package com.programmers.challenge.october;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Problem3Test {
+
+	@Test
+	public void solution() {
+		int[][] edges = {
+				{ 1, 2 },
+				{ 2, 3 },
+				{ 3, 4 }
+		};
+		assertThat(new Problem3().solution(4, edges)).isEqualTo(2);
+	}
+
+	@Test
+	public void solution3() {
+		int[][] edges = {
+				{ 1, 5 },
+				{ 2, 5 },
+				{ 3, 5 },
+				{ 4, 5 }
+		};
+
+		assertThat(new Problem3().solution(5, edges)).isEqualTo(2);
+	}
+}
