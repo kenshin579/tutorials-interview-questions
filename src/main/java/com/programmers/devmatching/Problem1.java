cf3f6d7861158f70debb8d244e22531174dda1e8
kenshin579 <kenshin579@hotmail.com>
2020-10-10 15:32:15 +0900

    [ALG-120] 응시 완료

---
+++ src/main/java/com/programmers/devmatching/Problem1.java
@@ -0,0 +1,16 @@
+package com.programmers.devmatching;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Problem1 {
+    /**
+     * 1. 고객치 전포에 도착하는 시간 순으로 키오스크와 매칭됨
+     * 2. 운용되지 않는 키오스크중에 운용되지 않는 시간이 가장 긴 키오스크와 매칭됨
+     * 2-1.운용되지 않은 시간이 가장 ㄱ킨
+     */
+    public int solution(int n, String[] customers) {
+        int answer = 0;
+        return answer;
+    }
+}
