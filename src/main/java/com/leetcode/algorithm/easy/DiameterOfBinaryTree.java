8451f3a6a46dfcc3964d83b62f3568e913a6173a
kenshin579 <kenshin579@hotmail.com>
2020-10-20 23:31:22 +0900

    [TIQ-141] 코멘트 추가

--- src/main/java/com/leetcode/algorithm/easy/DiameterOfBinaryTree.java
+++ src/main/java/com/leetcode/algorithm/easy/DiameterOfBinaryTree.java
@@ -9,10 +9,16 @@ import lombok.extern.slf4j.Slf4j;
  * Tags : Tree
  *
  * https://leetcode.com/problems/diameter-of-binary-tree/
+ *
+ * https://bonoogi.postype.com/post/7600170
  */
 @Slf4j
 @Deprecated
 public class DiameterOfBinaryTree {
+    /**
+     * Time Complexity :
+     * Algorithm :
+     */
     public int diameterOfBinaryTree(TreeNode root) {
         return 0;
     }
