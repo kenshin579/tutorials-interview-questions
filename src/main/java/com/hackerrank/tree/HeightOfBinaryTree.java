d909ddbe2d59d7db262621d8d9884fd835a5f34c
kenshin579 <kenshin579@hotmail.com>
2020-10-21 22:30:04 +0900

    [TIQ-141] HeightOfBinaryTree 스터디완료

--- src/main/java/com/hackerrank/tree/HeightOfBinaryTree.java
+++ src/main/java/com/hackerrank/tree/HeightOfBinaryTree.java
@@ -5,13 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * 높이(height) : 루트 노드에서 가장 깊숙히 있는 노드의 깊이
+ *
  * https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
  * https://tutorialspoint.dev/data-structure/graph-data-structure/roots-tree-gives-minimum-height
  */
 @Slf4j
 public class HeightOfBinaryTree {
+    /**
+     * https://stackoverflow.com/questions/2597637/finding-height-in-binary-search-tree
+     */
     public static int height(Node root) {
-        
-        return 0;
+        if (root == null) {
+            return -1;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int height = Math.max(leftHeight, rightHeight) + 1;
+        return height;
     }
 }
