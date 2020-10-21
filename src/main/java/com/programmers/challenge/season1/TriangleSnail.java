264c26da6c18eb264f46774595b330eda9de2ef3
kenshin579 <kenshin579@hotmail.com>
2020-10-21 23:13:49 +0900

    [TIQ-142] 삼각달팽이 init

---
+++ src/main/java/com/programmers/challenge/season1/TriangleSnail.java
@@ -0,0 +1,29 @@
+package com.programmers.challenge.season1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * 월간 코드 챌린지 시즌1 > 삼각 달팽이
+ * Difficulty : Easy
+ * Tags : DataStructure
+ * <p>
+ * https://programmers.co.kr/learn/courses/30/lessons/68645
+ * https://prgms.tistory.com/29
+ */
+@Slf4j
+public class TriangleSnail {
+    /**
+     * Time Complexity :
+     * <p>
+     * Algorithm :
+     */
+    public int[] solution(int n) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+}
