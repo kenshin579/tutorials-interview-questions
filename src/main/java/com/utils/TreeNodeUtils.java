4643563e7c8c31c7ed6ae1931e75ecfa889ddacb
kenshin579 <kenshin579@hotmail.com>
2020-10-21 22:44:35 +0900

    [TIQ-141] TreeNodeUtils height 추가

--- src/main/java/com/utils/TreeNodeUtils.java
+++ src/main/java/com/utils/TreeNodeUtils.java
@@ -10,4 +10,10 @@ public final class TreeNodeUtils {
         return new PreorderTraversal().preorderTraversal(root);
     }
 
+    public static int height(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
 }
