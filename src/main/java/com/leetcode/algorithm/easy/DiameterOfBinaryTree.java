96517ee7a0e3d2d89f4da539a6342f37d4226e68
kenshin579 <kenshin579@hotmail.com>
2020-10-22 21:39:01 +0900

    [ALG-141] 알고리즘 설명 추가

--- src/main/java/com/leetcode/algorithm/easy/DiameterOfBinaryTree.java
+++ src/main/java/com/leetcode/algorithm/easy/DiameterOfBinaryTree.java
@@ -19,9 +19,12 @@ import lombok.extern.slf4j.Slf4j;
 public class DiameterOfBinaryTree {
     /**
      * Time Complexity :
-     * Algorithm :
-     * 1.diameter를 계산
-     * 2.diameter
+     * Algorithm : 트리의 지름의 최대 값은 아래 2가지 케이스로 나뉜다
+     * Case1. root를 포함하는 경우
+     * - leftDepth + rightDepth를 합치면 된다
+     * Case2. root를 포함하지 않는 경우
+     * - 왼쪽, 오른쪽 중에 최대 값이 있다
+     * 최종 : Case1, Case2의 값중에 최대 값을 반환하면 된다
      */
     public int diameterOfBinaryTree(TreeNode root) {
         if (root == null)
