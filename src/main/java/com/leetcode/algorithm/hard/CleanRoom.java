4c5c8814903f51dcd4fd09be7fadaf53a6731f7c
kenshin579 <kenshin579@hotmail.com>
2020-10-24 14:05:38 +0900

    [TIQ-147] CleanRoom init

---
+++ src/main/java/com/leetcode/algorithm/hard/CleanRoom.java
@@ -0,0 +1,17 @@
+package com.leetcode.algorithm.hard;
+
+/**
+ * 489. Robot Room Cleaner
+ * Difficulty : Hard
+ * Tags : Graph
+ *
+ * https://leetcode.com/problems/robot-room-cleaner/
+ * https://linlaw0229.github.io/2019/03/02/489-Robot-Room-Cleaner/
+ * https://www.programmersought.com/article/3123180170/
+ * https://github.com/Cee/Leetcode/blob/master/489%20-%20Robot%20Room%20Cleaner.java
+ */
+public class CleanRoom {
+    public void cleanRoom(Robot robot) {
+
+    }
+}
