ba00fa71250cf3e72d1c5d93bf3abf0cac374093
kenshin579 <kenshin579@hotmail.com>
2020-10-25 19:31:05 +0900

    [TIQ-154] 게임 맵 최단거리 init

---
+++ src/main/java/com/programmers/master/GameMapShortestPath.java
@@ -0,0 +1,19 @@
+package com.programmers.master;
+
+/**
+ * 찾아라 프로그래밍 마에스터 > 게임 맵 최단거리
+ * Difficulty : Medium
+ * Tags : Graph
+ *
+ * https://programmers.co.kr/learn/courses/30/lessons/1844
+ */
+public class GameMapShortestPath {
+    /**
+     * Time Complexity :
+     * Algorithm :
+     */
+    public int solution(int[][] maps) {
+        int answer = 0;
+        return answer;
+    }
+}
