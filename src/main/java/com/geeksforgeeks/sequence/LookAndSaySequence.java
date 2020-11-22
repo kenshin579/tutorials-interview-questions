b7395d4e0fa043b2b145c08d0f1fc01c6e2a5cfd
kenshin579 <kenshin579@hotmail.com>
2020-11-22 20:19:16 +0900

    [TIQ-166] LookAndSaySequence init

---
+++ src/main/java/com/geeksforgeeks/sequence/LookAndSaySequence.java
@@ -0,0 +1,34 @@
+package com.geeksforgeeks.sequence;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Look and Say Sequence
+ * Difficulty : Easy
+ * Tags : Sequence
+ *
+ * https://www.geeksforgeeks.org/look-and-say-sequence/
+ * https://ko.wikipedia.org/wiki/%EC%9D%BD%EA%B3%A0_%EB%A7%90%ED%95%98%EA%B8%B0_%EC%88%98%EC%97%B4
+ */
+@Slf4j
+public class LookAndSaySequence {
+    /**
+     * Time Complexity :
+     *
+     */
+    public String lookAndSaySequence(int n) {
+        return "";
+    }
+
+    String countAndSay(int n) {
+        StringBuilder sb = new StringBuilder();
+        String numStr = Integer.toString(n);
+        int count;
+
+        for (int i = 0; i < numStr.length(); i++) {
+
+        }
+
+        return sb.toString();
+    }
+}
