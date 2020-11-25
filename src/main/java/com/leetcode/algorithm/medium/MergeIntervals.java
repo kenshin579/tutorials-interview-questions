7119f434fd0a6aa18c9273868b247ce508eadbdf
kenshin579 <kenshin579@hotmail.com>
2020-11-25 22:33:04 +0900

    [TIQ-188] MergeIntervals 스터디중

--- src/main/java/com/leetcode/algorithm/medium/MergeIntervals.java
+++ src/main/java/com/leetcode/algorithm/medium/MergeIntervals.java
@@ -10,43 +10,62 @@ import java.util.Stack;
  * 56. Merge Intervals
  * Difficulty : Medium
  * Tags : Array
- *
+ * <p>
  * https://leetcode.com/problems/merge-intervals/
  * https://www.geeksforgeeks.org/merging-intervals/
  */
 @Slf4j
 @Deprecated
 public class MergeIntervals {
-	/**
-	 * Time Complexity :
-	 *
-	 * Algorithm : Stack을 사용하는 방법
-	 *
-	 */
-	public int[][] merge(int[][] intervals) {
-		int[][] result = new int[intervals.length][2];
+    /**
+     * Time Complexity :
+     * <p>
+     * Algorithm : Stack을 사용하는 방법
+     */
+    public int[][] merge(int[][] intervals) {
+        Stack<int[]> stack = new Stack<>();
 
-		Stack<int[]> stack = new Stack<>();
+        //sort
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
 
-		//sort
-		Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        //stack에 쌓으면서 merge를 진행하면 됨
+        stack.push(intervals[0]);
+        int[] interval;
 
-		//stack에 쌓으면서 merge를 진행하면 됨
-		stack.push(intervals[0]);
-		int[] pop;
+        for (int i = 1; i < intervals.length; i++) {
+            interval = stack.pop();
 
-		for (int i = 1; i < intervals.length; i++) {
-			pop = stack.pop();
+            if (isPartiallyOverlapped(interval, intervals[i])) {
+                stack.push(new int[] {interval[0], intervals[i][1]});
+            } else if (contains(interval, intervals[i])) {
+                stack.push(interval);
+            } else {
+                stack.push(interval);
+                stack.push(intervals[i]);
+            }
 
-		}
+        }
 
-		//stack에서 뽑아서
+        //stack에서 뽑아서
+        return convertStackToArrays(stack);
+    }
 
-		return result;
-	}
+    int[][] convertStackToArrays(Stack<int[]> stack) {
+        int[][] result = new int[stack.size()][2];
+        int[] pop;
+        int index = stack.size() - 1;
+        while (!stack.isEmpty()) {
+            pop = stack.pop();
+            result[index--] = pop;
+        }
+        return result;
+    }
 
-	boolean isMutuallyExclusiveOverlapped(int[] x, int[] y) {
+    boolean isPartiallyOverlapped(int[] x, int[] y) {
+        return !contains(x, y) && y[0] > x[0] && y[0] < x[1];
+    }
 
-		return false;
-	}
+    boolean contains(int[] x, int[] y) {
+        return y[0] > x[0] && x[1] > y[1];
+    }
 }
--- src/test/java/com/leetcode/algorithm/medium/MergeIntervalsTest.java
+++ src/test/java/com/leetcode/algorithm/medium/MergeIntervalsTest.java
@@ -10,95 +10,119 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 public class MergeIntervalsTest {
 
-	@Test
-	public void merge() {
-		int[][] intervals = {
-				{ 1, 3 },
-				{ 2, 6 },
-				{ 8, 10 },
-				{ 15, 18 }
-		};
+    @Test
+    public void merge() {
+        int[][] intervals = {
+                {1, 3},
+                {2, 6},
+                {8, 10},
+                {15, 18}
+        };
 
-		int[][] result = {
-				{ 1, 6 },
-				{ 8, 10 },
-				{ 15, 18 }
-		};
+        int[][] result = {
+                {1, 6},
+                {8, 10},
+                {15, 18}
+        };
 
-		assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
-	}
+        assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
+    }
 
-	@Test
-	public void merge2() {
-		int[][] intervals = {
-				{ 1, 4 },
-				{ 4, 5 }
-		};
+    @Test
+    public void merge2() {
+        int[][] intervals = {
+                {1, 4},
+                {3, 5}
+        };
 
-		int[][] result = {
-				{ 1, 5 }
-		};
+        int[][] result = {
+                {1, 5}
+        };
 
-		assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
-	}
+        assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
+    }
 
-	@Test
-	public void merge3() {
-		int[][] intervals = {
-				{ 1, 4 },
-				{ 2, 3 }
-		};
+    @Test
+    public void merge3() {
+        int[][] intervals = {
+                {1, 4},
+                {2, 3}
+        };
 
-		int[][] result = {
-				{ 1, 4 }
-		};
+        int[][] result = {
+                {1, 4}
+        };
 
-		assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
-	}
+        assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
+    }
 
-	@Test
-	public void merge4() {
-		int[][] intervals = {
-				{ 1, 2 },
-				{ 3, 4 }
-		};
+    @Test
+    public void merge4() {
+        int[][] intervals = {
+                {1, 2},
+                {3, 4}
+        };
 
-		int[][] result = {
-				{ 1, 2 },
-				{ 3, 4 }
-		};
+        int[][] result = {
+                {1, 2},
+                {3, 4}
+        };
 
-		assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
-	}
+        assertThat(new MergeIntervals().merge(intervals)).isEqualTo(result);
+    }
 
-	@Test
-	public void isMutuallyExclusiveOverlapped() {
-		assertThat(new MergeIntervals().isMutuallyExclusiveOverlapped(new int[] {1, 3}, new int[] {3, 4})).isTrue();
-	}
+    @Test
+    public void convertStackToArrays() {
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{2, 3});
+        stack.push(new int[]{1, 4});
+        stack.push(new int[]{3, 1});
 
-	@Test
-	public void sort() {
-		int[][] intervals = {
-				{ 1, 4 },
-				{ 4, 5 },
-				{ 3, 4 }
-		};
+        int[][] result = new MergeIntervals().convertStackToArrays(stack);
+        assertThat(result.length).isEqualTo(3);
 
-		int[][] merge = new MergeIntervals().merge(intervals);
+        assertThat(result[0][0]).isEqualTo(2);
+        assertThat(result[1][0]).isEqualTo(1);
+        assertThat(result[2][0]).isEqualTo(3);
+    }
 
-		for (int i = 0; i < merge.length - 1; i++) {
-			assertThat(merge[i][0]).isLessThan(merge[i + 1][0]);
-		}
-	}
+    @Test
+    public void isPartiallyOverlapped() {
+        assertThat(new MergeIntervals().isPartiallyOverlapped(new int[]{1, 3}, new int[]{2, 4})).isTrue();
+        assertThat(new MergeIntervals().isPartiallyOverlapped(new int[]{1, 6}, new int[]{2, 4})).isFalse();
+        assertThat(new MergeIntervals().isPartiallyOverlapped(new int[]{1, 3}, new int[]{7, 9})).isFalse();
+    }
 
-	@Test
-	public void test_stack() {
-		Stack<int[]> stack = new Stack<>();
+    @Test
+    public void contains() {
+        assertThat(new MergeIntervals().contains(new int[]{1, 5}, new int[]{2, 4})).isTrue();
+        assertThat(new MergeIntervals().contains(new int[]{1, 5}, new int[]{3, 9})).isFalse();
+        assertThat(new MergeIntervals().contains(new int[]{1, 5}, new int[]{8, 10})).isFalse();
+    }
 
-		stack.push(new int[] { 2, 3 });
-		stack.push(new int[] { 1, 4 });
-		int[] pop = stack.pop();
-		assertThat(pop[0]).isEqualTo(1);
-		assertThat(pop[1]).isEqualTo(4);
-	}
+    @Test
+    public void sort() {
+        int[][] intervals = {
+                {1, 4},
+                {4, 5},
+                {3, 4}
+        };
+
+        int[][] merge = new MergeIntervals().merge(intervals);
+
+        for (int i = 0; i < merge.length - 1; i++) {
+            assertThat(merge[i][0]).isLessThan(merge[i + 1][0]);
+        }
+    }
+
+    @Test
+    public void test_stack() {
+        Stack<int[]> stack = new Stack<>();
+
+        stack.push(new int[]{2, 3});
+        stack.push(new int[]{1, 4});
+        int[] pop = stack.pop();
+        assertThat(pop[0]).isEqualTo(1);
+        assertThat(pop[1]).isEqualTo(4);
+    }
 }
