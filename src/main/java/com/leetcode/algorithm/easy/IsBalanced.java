66c15e78bcb04a72deb1bff7993417a3579cf984
kenshin579 <kenshin579@hotmail.com>
2020-12-06 23:15:57 +0900

    [TIQ-208] IsBalanced 스터디중

--- src/main/java/com/leetcode/algorithm/easy/IsBalanced.java
+++ src/main/java/com/leetcode/algorithm/easy/IsBalanced.java
@@ -1,12 +1,18 @@
 package com.leetcode.algorithm.easy;
 
 import com.leetcode.algorithm.common.struct.TreeNode;
+import lombok.extern.slf4j.Slf4j;
 
 /**
+ * 110. Balanced Binary Tree
+ * Difficulty : Easy
+ * Tags : Tree
+ *
  * https://leetcode.com/problems/balanced-binary-tree/
  * http://typeocaml.com/2014/11/26/height-depth-and-level-of-a-tree/
  * https://stackoverflow.com/questions/29326512/what-is-the-difference-between-the-height-of-a-tree-and-depth-of-a-tree
  */
+@Slf4j
 @Deprecated
 public class IsBalanced {
     public boolean isBalanced(TreeNode root) {
