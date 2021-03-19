b82392574bb169127816ea0dc3439519e1407fc4
kenshin579 <kenshin579@hotmail.com>
2021-03-19 12:45:45 +0900

    [TGO-223] n-ary tree level order traversal init

---
+++ go/leetcode/algorithm/medium/level_order/level_order.go
@@ -0,0 +1,11 @@
+package level_order
+
+/*
+ 429. N-ary Tree Level Order Traversal
+ Difficulty : Easy
+ Tags : String
+ https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+*/
+func levelOrder(root *Node) [][]int {
+
+}
